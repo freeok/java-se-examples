ee311cd32113aee5d8af709f1efbcf31214611a1
freeok <51998152+freeok@users.noreply.github.com>
2024-11-07 23:43:58 +0800

    代码优化

--- experimental/src/main/java/org/pcdd/javase/rule34vault/Rule34vault.java
+++ experimental/src/main/java/org/pcdd/javase/rule34vault/Rule34vault.java
@@ -1,6 +1,7 @@
 package org.pcdd.javase.rule34vault;
 
 import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.RandomUtil;
 import cn.hutool.http.HttpRequest;
 import cn.hutool.http.HttpResponse;
 import cn.hutool.http.HttpUtil;
@@ -18,20 +19,27 @@ import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class Main {
+public class Rule34vault {
 
     public static final String USER_ID = "66608";
     public static final String detailsUrl = "https://rule34vault.com/post/";
     public static final Proxy PROXY = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("127.0.0.1", 7897));
+    public static final String destPath = "E:/Downloads/403/rule34vault/";
+    public static final int timeout = 60_000;
 
-    public static void main(String[] args) {
+    @SneakyThrows
+    void main() {
         Set<String> urls = getBookmarks();
-        System.out.println("书签数量 " + urls.size());
+        System.out.println("云书签数量 " + urls.size());
+        // 去重，本地已存在的不再下载
+        urls.removeIf(url -> FileUtil.exist(destPath, FileUtil.getName(url) + "\\.(jpg|mp4|png|gif)"));
+        System.out.println("待下载数量 " + urls.size());
 
-        ExecutorService executor = Executors.newFixedThreadPool(2);
+        ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor();
         // 爬取书签图片
         for (String url : urls) {
             executor.execute(() -> crawl(url));
+            Thread.sleep(RandomUtil.randomInt(50, 300));
         }
 
         executor.shutdown();
@@ -42,20 +50,32 @@ public class Main {
     public static void crawl(String url) {
         String s = HttpUtil.createGet(url)
                 .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.5410.0 Safari/537.36")
-                .timeout(3000)
+                .timeout(timeout)
                 .execute()
                 .body();
 
         Document document = Jsoup.parse(s);
         Elements img = document.select("body > app-root > app-root-layout-page > div > mat-sidenav-container > mat-sidenav-content > app-post-page > app-page > app-post-page-content > app-post-image > div > img");
-        String imgSrc = img.attr("src").replace(".small.jpg", ".jpg");
-        System.out.println("正在爬取 " + imgSrc);
-        download(imgSrc, STR."E:/Downloads/403/image/test/\{FileUtil.getName(imgSrc)}");
+        Elements video = document.select("body > app-root > app-root-layout-page > div > mat-sidenav-container > mat-sidenav-content > app-post-page > app-page > app-post-page-content > app-post-image > div > div > video > source");
+        String src;
+
+        if (!img.isEmpty()) {
+            src = img.attr("src").replace("small.jpg", "jpg");
+        } else if (!video.isEmpty()) {
+            src = video.attr("src").replace("720.mp4", "mp4");
+        } else {
+            System.out.println("未找到视频或图片！");
+            return;
+        }
+
+        System.out.println("正在下载 " + src);
+        download(src, STR."\{destPath}\{FileUtil.getName(src)}");
     }
 
     /**
      * 获取书签里的全部 url
      */
+    @SneakyThrows
     public static Set<String> getBookmarks() {
         int skip = 0; // 从第几条开始
         int take = 100; // 查询数
@@ -63,6 +83,7 @@ public class Main {
 
         while (true) {
             String s = reqBookmarks(skip, take);
+            Thread.sleep(RandomUtil.randomInt(100, 300));
             JSONObject entries = JSONUtil.parseObj(s);
             int totalCount = entries.get("totalCount", Integer.class);
             Object items = entries.get("items");
@@ -94,6 +115,7 @@ public class Main {
         HttpResponse resp = HttpUtil.createPost(bookmarkUrl)
                 .contentType("application/json")
                 .body(body)
+                .timeout(timeout)
                 .execute();
 
         return resp.body();
@@ -105,7 +127,7 @@ public class Main {
     public static void download(String url, String dest) {
         HttpRequest req = HttpRequest.get(url)
                 .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.5410.0 Safari/537.36")
-                .timeout(3000);
+                .timeout(timeout);
         req.setProxy(PROXY);
         FileUtil.writeBytes(req.execute().bodyBytes(), dest);
     }
--- leetcode/src/main/java/org/pcdd/leetcode/Solution.java
+++ leetcode/src/main/java/org/pcdd/leetcode/Solution.java
@@ -1,5 +1,19 @@
 package org.pcdd.leetcode;
 
 class Solution {
+    public int maxArea(int[] height) {
+        int max = 0;
+        int i = 0;
+        int j = height.length - 1;
 
+        while (i < j) {
+            // height * width
+            int area = Math.min(height[i], height[j]) * (j - i);
+            max = Math.max(area, max);
+            if (height[i] < height[j]) i++;
+            else j--;
+        }
+
+        return max;
+    }
 }
